fdd4c2b159d220fd5c1ef35510fbc112e74a0a62
Trey Yu <treyyu168@gmail.com>
2023-12-12 19:28:44 -0700

    Removed OpenMeteo Library, wrote Fetch call, processed Weather data, created Forecast List

--- react-app/src/components/Weather.tsx
+++ react-app/src/components/Weather.tsx
@@ -1,52 +1,88 @@
 import {useEffect, useState} from "react";
-import {fetchWeatherApi} from "openmeteo";
 import cloudIcon from "../assets/icons/02n.png"
 
-const params = {
-    "latitude" : 39.5539,
-    "longitude": -104.9694,
-    "current": ["temperature_2m", "weather_code"],
-    "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min"],
-    "temperature_unit": "fahrenheit",
-    "wind_speed_unit": "mph",
-    "precipitation_unit": "inch",
-    "timezone": "America/Denver"
+interface ForecastDaily {
+    temperature_2m_min: number[],
+    temperature_2m_max: number[],
+    time: string[],
+    weather_code: number[]
 }
 
-const url = "https://api.open-meteo.com/v1/gfs";
-const responses = await fetchWeatherApi(url, params);
-const response = responses[0]
+interface ProcessedForecast {
+    date: string,
+    high: number,
+    low: number,
+    weather_code: number
+}
 
-function processWeatherData() {
-    const utcOffsetSeconds = response.utcOffsetSeconds();
-    const current = response.current()!;
+function addTime(date: string): string {
+    let slowTime = new Date(date)
+    let correctedTime: number = slowTime.getTime() + 25200000
 
-    return {
-        current: {
-            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-            temperature: Math.trunc(current.variables(0)!.value()),
-            weatherCode: current.variables(1)!.value()
+    return new Date(correctedTime).toDateString().substring(0, 10)
+}
+function processForecast(data: ForecastDaily): ProcessedForecast[] {
+    let results = []
+
+    for(let i: number = 0; i < data.time.length; i++) {
+        let dailyForecast: ProcessedForecast = {
+            'date': addTime(data.time[i]),
+            'high': data.temperature_2m_max[i],
+            'low': data.temperature_2m_min[i],
+            'weather_code': data.weather_code[i]
         }
+
+        results.push(dailyForecast)
     }
+
+    return results
 }
 
-const current = processWeatherData()
 const Weather = () => {
-    const [temperature, setTemperature] = useState(current.current.temperature)
-    const [weatherCondition, setWeatherCondition] = useState(-1)
+
+    const defaultForecast = {
+        'date': '',
+        'high': -1,
+        'low': -1,
+        'weather_code': -1
+    }
+    const [currentTemperature, setCurrentTemperature] = useState(-1)
+    // const [todayHigh, setTodayHigh] = useState(-1)
+    // const [todayLow, setTodayLow] = useState(-1)
+    // const [weatherCondition, setWeatherCondition] = useState(-1)
+    const [forecast, setForecast] = useState([defaultForecast])
+
+    const getWeatherAPI = () => {
+        fetch('https://api.open-meteo.com/v1/gfs?latitude=39.5539&longitude=-104.9694&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FDenver')
+        .then((response) => {
+            return response.json()
+        })
+        .then(data => {
+            setCurrentTemperature(data.current.temperature_2m)
+            // setWeatherCondition(data.current.weather_code)
+            setForecast(processForecast(data.daily))
+        })
+    }
+
 
     useEffect(() => {
         setInterval(() => {
-            const current = processWeatherData()
-            setTemperature(current.current.temperature)
-            setWeatherCondition(current.current.weatherCode) //TODO: Create function to update every 5 mins
+            getWeatherAPI()
         }, 300000)
     })
+
     return <>
         <h1>Weather</h1>
         {/*<h2>{weatherCondition}</h2>*/}
         <img src={cloudIcon} alt={"weatherCondition"}/>
-        <h2>{temperature + "°F"}</h2>
+        <h2>{currentTemperature + "°F"}</h2>
+        {forecast.map((item) => (
+            <div className='list-group-item'
+                key={item.date}>
+                {item.date} High: {item.high}°F Low: {item.low}°F
+            </div>
+        ))}
+        <button onClick={getWeatherAPI}>Click</button>
     </>
 }
 
